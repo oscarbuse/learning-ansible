9a0b5220736ba1d38e2b930e65a5f6f539a376b0
oscarbuse <oscar@kwalinux.nl>
2020-06-09 11:20:15 +0200

    added package jq

--- bootstrap1.sh
+++ bootstrap1.sh
@@ -7,6 +7,7 @@ apt -y update
 apt -y dist-upgrade
 apt install -y ansible
 apt install -y python3-distutils
+apt install -y jq
 apt install -y sshpass
 apt install -y lynx
 echo "192.168.66.62 ans2" >> /etc/hosts
